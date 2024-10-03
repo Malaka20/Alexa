0efe61c15eeb2255417b0430025875bdd75bfd46
Malaka20/NETHU-MD-V1 2 <153556272+Malaka20@users.noreply.github.com>
2024-10-03 11:52:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "AcAXhIBZ#61n0476sEyV-MECMX_rnEVv1SpY64-z5XG28QirkqSs",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
